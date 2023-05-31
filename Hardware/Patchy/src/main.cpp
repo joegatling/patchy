e2de3e0f5a3409c1201388bfdcf0bec07c4a90bb
Joe Gatling <joe.gatling@gmail.com>
2023-05-31 09:09:30 -0700

    Simplified the circuit (bonus, it now works)

--- Hardware/Patchy/src/main.cpp
+++ Hardware/Patchy/src/main.cpp
@@ -6,23 +6,22 @@
 #include "Patchboard.h"
 #include "Game.h"
 
+#define SHOW_PATCH_DEBUG 0
+
+#if SHOW_PATCH_DEBUG
+    #define PATCH_PRINTLN(x) Serial.println(x);
+    #define PATCH_PRINT(x) Serial.print(x);
+#else
+    #define PATCH_PRINTLN(x) ((void)0)
+    #define PATCH_PRINT(x) ((void)0)
+#endif
+
+
 #if NODEMCU
-    #define SERIAL_OUT_PIN      D0
-
-    #define LATCH_PIN           D5
-    #define CLOCK_PIN           D6
-    #define CLEAR_PIN           D7
-
     #define LED_PIN             D1       
 #endif
 
 #if UNO
-    #define SERIAL_OUT_PIN      7
-
-    #define LATCH_PIN           6
-    #define CLOCK_PIN           5
-    #define CLEAR_PIN           4
-
     #define LED_PIN             8
 #endif
 
@@ -41,44 +40,33 @@ Game game(PANEL_ROWS, PANEL_COLUMNS, strip);
 
 void updateInput()
 {
-    // Step 1 - Shift the ouput pin. At each update there is a single plug
-    // set HIGH. We can then test the voltage on each other plug to determine 
-    // if any have been connected.
+    // Step 1 - Increment the current pin and set that one to LOW
 
-    digitalWrite(LATCH_PIN, LOW);
-    digitalWrite(CLOCK_PIN, HIGH);
-    digitalWrite(CLOCK_PIN, LOW);
-    digitalWrite(LATCH_PIN, HIGH);
-
-    // This keeps track of which plug is currently HIGH
     currentPlug = (currentPlug + 1) % PLUG_COUNT;
-    //currentPlug = 0;
 
-    Serial.print("Current plug: ");
-    Serial.println(currentPlug);
+    // Serial.print("Current plug: ");
+    // Serial.println(currentPlug);
 
     mcp[currentPlug/8].pinMode(currentPlug%8, OUTPUT);
-    mcp[currentPlug/8].digitalWrite(currentPlug%8, HIGH);
+    mcp[currentPlug/8].digitalWrite(currentPlug%8, LOW);
 
-    // Step 2 - Loop through all the input pins. The pin corresponding to the output is expected
-    // to be high. Any other pin that is high must have been patched.
+    // Step 2 - Loop through all the input pins. The current pin is LOW, and other LOW
+    // pins must be connected to it.
     int connectedPlug = -1;
     
-  //  Serial.println("Connections: ");
-
     for(unsigned int i = 0; i < PLUG_COUNT; i++)
     {
-        //if(i != currentPlug)
-        if(true)
+        if(i != currentPlug)        
         {
             uint8_t val = mcp[i/8].digitalRead(i%8);
-            Serial.print(val);
+            
+            PATCH_PRINT(val == HIGH ? "_" : "^" );
 
             if(val == 0)
             {
                 
                 connectedPlug = i;
-                //break;
+                break;
                 // Working on a 1-to-1 assumption, once we have found a single connection
                 // we can break out of the loop early.
             }
@@ -86,56 +74,21 @@ void updateInput()
         }
         else
         {
-            Serial.print("_");
+            PATCH_PRINT("*");
         }
-
         if(i%8 == 7)
         {
-            Serial.print(" ");
+            PATCH_PRINT(" ");
         }
     }
-    Serial.println("");
-
-    mcp[currentPlug/8].pinMode(currentPlug%8, INPUT);
-    //mcp[currentPlug/8].digitalWrite(currentPlug%8, HIGH);
-    //Serial.println("");
     
+    PATCH_PRINTLN("");
+
+    mcp[currentPlug/8].pinMode(currentPlug%8, INPUT_PULLUP);    
 
     patchboard.SetConnection(currentPlug, connectedPlug);
 }
 
-void initOutputCycle()
-{
-    digitalWrite(CLEAR_PIN, HIGH);
-    digitalWrite(LATCH_PIN, LOW);
-
-    int mcuCount = PLUG_COUNT/8;
-    for(int i = 0; i < mcuCount; ++i)
-    {
-        //uint8_t val = i == (mcuCount-1) ? 1 : 0;
-        //uint8_t val = i == (mcuCount-1) ? ~1 : ~0;
-        uint8_t val = 0;
-        Serial.print("Output: ");
-        
-        for (int i = 7; i >= 0; i--)
-        {
-            bool b = bitRead(val, i);
-            Serial.print(b);
-        }
-
-        Serial.println("");
-        shiftOut(SERIAL_OUT_PIN, CLOCK_PIN, MSBFIRST, val);
-    }
-
-    digitalWrite(LATCH_PIN, HIGH);
-    digitalWrite(CLOCK_PIN, LOW);
-    
-    // Set the serial output pin to input. This makes it floating so that
-    // it can be driven by the output of the shift register.
-    pinMode(SERIAL_OUT_PIN, INPUT);
-
-    currentPlug = 0;
-}
 
 void onConnectionChanged(int plugA, int plugB, bool isConnected)
 {
@@ -168,16 +121,10 @@ void onConnectionChanged(int plugA, int plugB, bool isConnected)
 
 void setup() 
 {
-#if KIT32
-	Heltec.begin(true, false, true);
-	Wire.begin(SDA_OLED, SCL_OLED); //Scan OLED's I2C address via I2C0
-#else    
     Serial.begin(115200);
-#endif
 
     Serial.println("Patchy Test");
 
-    //u8g2.begin();
     bool mcpError = false;
 
     for(int i = 0; i < PLUG_COUNT / 8; ++i)
@@ -188,7 +135,6 @@ void setup()
             Serial.print(i); 
             Serial.println(" Error.");
             mcpError = true;
-            //while (1);
         }
     }
 
@@ -200,30 +146,20 @@ void setup()
         }
     }
 
-    // // Set up input pins
+    // Set up input pins
     for(int i = 0; i < PLUG_COUNT; i++)
     {
-        mcp[i/8].pinMode(i%8, INPUT);
+        mcp[i/8].pinMode(i%8, INPUT_PULLUP);
     }
-
-    // Set up output cycler
-    pinMode(LATCH_PIN, OUTPUT);
-    pinMode(CLOCK_PIN, OUTPUT);
-    pinMode(CLEAR_PIN, OUTPUT);
-    pinMode(SERIAL_OUT_PIN, OUTPUT);
-
-    initOutputCycle();
 
     patchboard.SetConnectionCallback(onConnectionChanged);
     
-   // game.Initialize();
+   game.Initialize();
 }
 
 void loop() 
 {
     updateInput();
     patchboard.Update();
-
-    delay(500);
-    //game.Update();
+    game.Update();
 }
